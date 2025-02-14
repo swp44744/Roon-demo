74efaa0171ede5092d1ade7f98dd8bbd50d53d54
sraut1 <swapnil.raut@autograph.io>
2025-02-13 21:27:37 -0800

    adding clear button to the search input

--- components/Experts/SearchExpert.tsx
+++ components/Experts/SearchExpert.tsx
@@ -1,20 +1,14 @@
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, Pressable } from "react-native";
 import React, { useEffect, useState } from "react";
 import Animated from "react-native-reanimated";
 import { useDebounce } from "use-debounce";
 import { useExperts } from "@/hooks/useExperts";
+import { Ionicons } from "@expo/vector-icons";
 
 const SearchExpert = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
-
-  const handleInputChange = (text: string) => {
-    setSearchTerm(text);
-  };
-
-  const { refetch } = useExperts({
-    specialty: debouncedSearchTerm || "",
-  });
+  const { refetch } = useExperts({ specialty: debouncedSearchTerm || "" });
 
   useEffect(() => {
     if (debouncedSearchTerm !== undefined) {
@@ -24,15 +18,22 @@ const SearchExpert = () => {
 
   return (
     <Animated.View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Search Expert"
-        placeholderTextColor="#fff"
-        autoCorrect={false}
-        autoCapitalize="none"
-        value={searchTerm}
-        onChangeText={handleInputChange}
-      />
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search Expert"
+          placeholderTextColor="#fff"
+          autoCorrect={false}
+          autoCapitalize="none"
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+        />
+        {searchTerm.length > 0 && (
+          <Pressable onPress={() => setSearchTerm("")} style={styles.clearButton}>
+            <Ionicons name="close-circle" size={22} color="#bbb" />
+          </Pressable>
+        )}
+      </View>
     </Animated.View>
   );
 };
@@ -42,20 +43,26 @@ const styles = StyleSheet.create({
     marginTop: 16,
     paddingHorizontal: 10,
   },
-  input: {
-    height: 50,
-    width: "100%",
-    borderRadius: 10,
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: "#23248d",
+    borderRadius: 10,
     paddingHorizontal: 15,
-    color: "white",
-    fontSize: 16,
-
+    height: 50,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
+  },
+  input: {
+    flex: 1,
+    color: "white",
+    fontSize: 16,
+  },
+  clearButton: {
+    marginLeft: 10, // Spacing between input and button
   },
 });
 
